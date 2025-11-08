810200a6646cd862113f42cfb4ce7460ac31b9cd
SA SHARIF KHAN <sharifbmdd@gmail.com>
2025-11-08 15:42:40 +0600

    Update

--- lib/services/location_services/getcurrentlocation.dart
+++ lib/services/location_services/getcurrentlocation.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:geolocator/geolocator.dart';
-// import 'package:geocoding/geocoding.dart';
 import 'package:http/http.dart' as http;
 
 class LocationService {
